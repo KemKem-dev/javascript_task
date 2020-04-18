64eaaf595b8983c1c67789bf315bb7dff731f69d
Oluwakemi Adeleke <61542846+KemKem-dev@users.noreply.github.com>
2020-04-18 11:57:34 +0100

    Update app.js
    Update this file.

--- app.js
+++ app.js
@@ -1,36 +1,24 @@
-let N;
-let foo = new Array();
-
-
-function myArr(N) {
-    for (let i = 1; i <= N; i++) {
-        if (i % 2 == 0) {
-            r = "yu";
-            foo.push(r)
-        } else if (i % 3 == 0) {
-            r = "gi";
-            foo.push(r)
-        } else if (i % 5 == 0) {
-            r = "oh";
-            foo.push(r)
-        } else {
-            foo.push(i)
-        };
-
-
-        if (i % 2 == 0) {
-            if (i % 3 == 0) {
-                r = "yu-gi"
-                foo.push(r)
-            } else if (i % 5 == 0) {
-                r = "yu-oh"
-                foo.push(r)
-            }
-            r = "yu"
-            foo.push(r)
-        };
-    };
-    return foo;
+function funnyNumberReplacer(maxNumber = 100) {
+    let minNumber = 0
+    let numberRange = [...Array(maxNumber).keys()].map(n => n + 1)
+    let output = [];
+    numberRange.forEach((number) => {
+        let input = ''
+        if (number % 2 === 0) {
+            input = 'yu'
+        }
+        if (number % 3 === 0) {
+            input += input ? '-gi' : 'gi'
+        }
+        if (number % 5 === 0) {
+            input += input ? '-oh' : 'oh'
+        }
+        if (!input) {
+            input = number
+        }
+        output.push(input)
+    });
+    return output
 };
-myArr(100);
-myArr(50);
+funnyNumberReplacer(100);
+funnyNumberReplacer(80);
